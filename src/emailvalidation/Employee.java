c05386c2bb6049ff8c22f67838fbbf5c7f9232c5
Nicholas Nicholson <nicholas.nicholson@hcl.com>
2020-11-24 16:04:13 -0600

    Commit: Populate email array method

--- src/emailvalidation/Employee.java
+++ src/emailvalidation/Employee.java
@@ -2,6 +2,35 @@ package emailvalidation;
 
 public class Employee 
 {
-	//private String employeeEmail;
+	private String employeeEmail;
+	
+	
+
+	public Employee(String employeeEmail) 
+	{
+		this.employeeEmail = employeeEmail;
+	}
+
+	public String getEmployeeEmail() {
+		return employeeEmail;
+	}
+
+	public void setEmployeeEmail(String employeeEmail) {
+		this.employeeEmail = employeeEmail;
+	}
+	public static void PopulateEmailArray(Employee[] empArray) 
+	{
+		String[] testEmails = new String[] {"kmiller@optonline.net", "sethbrown@comcast.net"
+				, "fwitness@me.com", "policies@yahoo.com", "marioph@yahoo.ca"
+				, "jsbach@mac.com", "thurston@aol.com", "monkeydo@outlook.com"
+				, "lbecchi@live.com", "nimaclea@me.com", "jelmer@sbcglobal.net"
+				, "lukka@aol.com"}; //12 Random Test E-mails
+		
+		for (int x = 0; x < empArray.length; x++) 
+		{
+			empArray[x] = new Employee(testEmails[x]);
+		}		
+		System.out.println("Email Array Populated");
+	}
 
 }
--- src/emailvalidation/ValidateEmails.java
+++ src/emailvalidation/ValidateEmails.java
@@ -1,10 +1,43 @@
 package emailvalidation;
+import java.util.Scanner;
+
 
 public class ValidateEmails {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Employee[] employeeEmails = new Employee[10];
+		Scanner userInput = new Scanner(System.in);
+		
+		//Populate array with test emails
+		Employee.PopulateEmailArray(employeeEmails);
+		//Give user option to list emails in system
+		//Get input from user
+		System.out.println("Enter employee email for system to search:");
+		//
+			
 
 	}
 
 }
+/*
+ * Project objective:
+ * As a developer, write a program to search a string entered by a user from the
+ * array of strings.
+ * 
+ * Background of the problem statement:
+ * You have an array of email IDs of employees. As a programmer, write a program
+ * to search the email ID entered by a user.
+ * 
+ * You must use the following:
+ * 
+ * Eclipse/IntelliJ: An IDE to code the application Java: A programming language
+ * Git: To connect and push files from the local system to GitHub GitHub: To
+ * store the application code and track its versions Core Java concepts:
+ * Methods, collections, and strings
+ * 
+ * Following requirements should be met:
+ * 
+ * The versions of the code should be tracked on GitHub repositories The code
+ * should be able to search the required string from the array of strings
+ */
